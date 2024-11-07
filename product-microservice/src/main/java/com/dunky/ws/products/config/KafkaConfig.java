a2394bb6ba8b0f88810440bfb31342ae9942017f
dunky-star <dunkygeoffrey39@gmail.com>
2024-11-07 16:17:57 -0700

    Kafka producer configuraton properties

--- product-microservice/src/main/java/com/dunky/ws/products/config/KafkaConfig.java
+++ product-microservice/src/main/java/com/dunky/ws/products/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.dunky.ws.products.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
@@ -9,6 +10,7 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Bean
     NewTopic createTopic() {
         return TopicBuilder.name("product-created-events-topic")
                 .partitions(3) // --> Increases scalability and it means 3 microservices can consume from this topic
