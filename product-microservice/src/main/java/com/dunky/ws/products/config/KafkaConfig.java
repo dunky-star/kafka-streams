a11c4bf3242e266f1d3fc8db8b02c85fdb16f456
dunky-star <dunkygeoffrey39@gmail.com>
2024-11-07 16:03:42 -0700

    Kafka producer configuraton properties

---
+++ product-microservice/src/main/java/com/dunky/ws/products/config/KafkaConfig.java
@@ -0,0 +1,20 @@
+package com.dunky.ws.products.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    NewTopic createTopic() {
+        return TopicBuilder.name("product-created-events-topic")
+                .partitions(3) // --> Increases scalability and it means 3 microservices can consume from this topic
+                .replicas(2) // --> Should be number of Kafka brokers in the cluster
+                .configs(Map.of("min.insync.replicas", "1")) // --> Since there are two Kafka brokers
+                .build();
+    }
+
+}
---
+++ product-microservice/src/test/java/com/dunky/ws/products/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunky.ws.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
