d376cf0835863b58bad01123b5992d95ea1c8bb9
dunky-star <dunkygeoffrey39@gmail.com>
2024-11-07 20:35:32 -0700

    Publishing event to Kafka topic from Spring Boot

--- product-microservice/src/main/java/com/dunky/ws/products/rest/ProductController.java
+++ product-microservice/src/main/java/com/dunky/ws/products/rest/ProductController.java
@@ -1,7 +1,10 @@
 package com.dunky.ws.products.rest;
 
+import com.dunky.ws.products.CustomException;
 import com.dunky.ws.products.entity.CreateProductRestModel;
 import com.dunky.ws.products.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,18 +12,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
     private final ProductService productService;
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     @PostMapping
-    public ResponseEntity<String> createProduct(@RequestBody CreateProductRestModel product) {
-        String productId = productService.createProduct(product);
+    public ResponseEntity<Object> createProduct(@RequestBody CreateProductRestModel product) {
+        String productId;
+        try {
+            productId = productService.createProduct(product);
+        }catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new CustomException(new Date(), e.getMessage(), "/api/v1/products" ));
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(productId);
     }
 }
