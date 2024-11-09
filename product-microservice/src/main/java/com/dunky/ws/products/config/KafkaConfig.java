1ce4c10cb7fd5b7fef7a51774afb404b674d8e19
dunky-star <dunkygeoffrey39@gmail.com>
2024-11-08 21:24:38 -0700

    Configuring acknowledgement and retires by Kafka producer

--- product-microservice/src/main/java/com/dunky/ws/products/config/KafkaConfig.java
+++ product-microservice/src/main/java/com/dunky/ws/products/config/KafkaConfig.java
@@ -15,6 +15,7 @@ public class KafkaConfig {
         return TopicBuilder.name("product-created-events-topic")
                 .partitions(3) // --> Increases scalability and it means 3 microservices can consume from this topic
                 .replicas(2) // --> Should be number of Kafka brokers in the cluster
+                // Minimum in-sync replicas brokers configuration
                 .configs(Map.of("min.insync.replicas", "1")) // --> Since there are two Kafka brokers
                 .build();
     }
