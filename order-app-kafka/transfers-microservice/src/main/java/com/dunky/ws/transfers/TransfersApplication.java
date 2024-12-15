34b012505e0fdebf15c59020b61725b13b0e2a5f
dunky-star <dunkygeoffrey39@gmail.com>
2024-12-15 02:33:02 -0700

    Transactions in Kafka

--- order-app-kafka/transfers-microservice/src/main/java/com/dunky/ws/transfers/TransfersApplication.java
+++ order-app-kafka/transfers-microservice/src/main/java/com/dunky/ws/transfers/TransfersApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class PaymentsApplication {
+public class TransfersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentsApplication.class, args);
+		SpringApplication.run(TransfersApplication.class, args);
 	}
 
 	@Bean
