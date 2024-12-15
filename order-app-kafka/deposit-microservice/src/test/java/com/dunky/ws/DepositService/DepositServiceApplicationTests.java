6f52897156ed90d663918c63f4fa540ef8db3a87
dunky-star <dunkygeoffrey39@gmail.com>
2024-12-15 01:42:37 -0700

    Transactions in Kafka

---
+++ order-app-kafka/deposit-microservice/src/test/java/com/dunky/ws/DepositService/DepositServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunky.ws.DepositService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
